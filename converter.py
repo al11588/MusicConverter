2761eee8db6958efacae84db9de957ced5dc3701
Alvin Lawson <al11588@gmail.com>
2016-10-18 02:14:54 -0400

    fixing a function

--- converter.py
+++ converter.py
@@ -2,9 +2,9 @@ import progressbar #progress bar library
 import time #time library
 from pydub import AudioSegment #converter library
 
-sound = AudioSegment.from_file("test1.mp3") #test1.mp3 is being converted
-
-sound.export("new.mp3", format="mp3", bitrate="128k") #new.mp3 is what it is going to be converted to.
+def export(sound,timeremaining):
+	sound = AudioSegment.from_file("test1.mp3") #test1.mp3 is being converted
+	sound.export("new.mp3", format="mp3", bitrate="128k") #new.mp3 is what it is going to be converted to.
 
 timeremaining = progressbar.ProgressBar() #outputs the time remaining to get converted.
 for i in timeremaining(range(80)):
